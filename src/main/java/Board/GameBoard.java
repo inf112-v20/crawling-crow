014d67291effd81861b442bb66807e6ee4b000c6
Thomas Husebø <thomas.husebo.90@gmail.com>
2020-02-06 10:28:49 +0100

    Made sure gameboard pass failing tests
    Co-authored-by: adipid <tim@adipid.no>
    Co-authored-by: thomashusebo <thomas.husebo.90@gmail.com>
    Co-authored-by: buzzhead <sigmundvolden@gmail.com>
    Co-authored-by: puj009 <puj009@uib.no>
    Co-authored-by: ljo071 <lise.johansen@student.uib.no>

--- src/main/java/Board/GameBoard.java
+++ src/main/java/Board/GameBoard.java
@@ -1,19 +1,22 @@
 package Board;
 
 public class GameBoard implements IGameBoard {
+    private int width;
+    private int height;
 
     // NOTE: (0,0) is top left corner
     public GameBoard(int height, int width) {
-
+        this.height = height;
+        this.width = width;
     }
 
     @Override
     public int getWidth() {
-        return 0;
+        return width;
     }
 
     @Override
     public int getHeight() {
-        return 0;
+        return height;
     }
 }
