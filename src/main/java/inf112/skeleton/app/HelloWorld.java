2e29e649acd773f016309a410924f6add68f6ff3
Thomas Husebø <thomas.husebo.90@gmail.com>
2020-02-06 15:13:52 +0100

    Completed Libgdx tutorial with some minor adjustments for the assignment #10
    Co-authored-by: adipid <tim@adipid.no>
    Co-authored-by: buzzhead <sigmundvolden@gmail.com>
    Co-authored-by: ljo071 <lise.johansen@student.uib.no>

--- src/main/java/inf112/skeleton/app/HelloWorld.java
+++ src/main/java/inf112/skeleton/app/HelloWorld.java
@@ -4,24 +4,58 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 public class HelloWorld implements ApplicationListener {
-    private SpriteBatch batch;
-    private BitmapFont font;
+    // Legacy Code
+    //private SpriteBatch batch;
+    //private BitmapFont font;
+
+    // We added these to represent the map and the maps layers
+    private TiledMap tiledMap;
+    // TODO: implement this
+    //private TiledMapTileLayer floorLayer;
+    //private TiledMapTileLayer robot;
+    //private TiledMapTileLayer hole;
+    //private TiledMapTileLayer flag;
+    private OrthogonalTiledMapRenderer mapRenderer;
+    private OrthographicCamera camera;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        font = new BitmapFont();
-        font.setColor(Color.RED);
+        // Legacy Code
+        //batch = new SpriteBatch();
+        //font = new BitmapFont();
+        //font.setColor(Color.RED);
+
+        // Initialize map from file
+        tiledMap = new TmxMapLoader().load("testMap001.tmx");
+
+        // Initialize layers
+        //TODO: Implement
+        //floorLayer = (TiledMapTileLayer) tiledMap.getLayers().get("FloorLayer");
+
+        // Initialize the camera
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.update();
+
+        // Initialize the map renderer
+        mapRenderer = new OrthogonalTiledMapRenderer(tiledMap, 1/6f);
+        mapRenderer.setView(camera);
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        font.dispose();
+        // Legacy Code
+        //batch.dispose();
+        //font.dispose();
     }
 
     @Override
@@ -29,9 +63,14 @@ public class HelloWorld implements ApplicationListener {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        batch.begin();
-        font.draw(batch, "Hello World", 200, 200);
-        batch.end();
+        // Update and Render Map
+        camera.update();
+        mapRenderer.render();
+
+        // Legacy Code
+        //batch.begin();
+        //font.draw(batch, "Hello World", 200, 200);
+        //batch.end();
     }
 
     @Override
