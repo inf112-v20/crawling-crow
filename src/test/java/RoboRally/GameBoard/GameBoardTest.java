e94d4dbca0ae2c9f8688b60b5a59b51695193c31
ljo071 <ljo071@users.noreply.github.com>
2020-02-07 13:39:59 +0100

    Added test for gameboard #13
    Co-authored-by: puj009 <puj009@uib.no>
    Co-authored-by: buzzdead <sigmundvolden@gmail.com>
    Co-authored-by: adipid <tim@adipid.no>

---
+++ src/test/java/RoboRally/GameBoard/GameBoardTest.java
@@ -0,0 +1,13 @@
+package RoboRally.GameBoard;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameBoardTest {
+
+    @Test
+    public void sizeOfGameBoard() {
+        assertEquals(GameBoard.TILE_SIZE, 300);
+    }
+}
