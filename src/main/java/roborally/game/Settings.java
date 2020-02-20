115f68d2953804b49999459decff13ff29c6d765
Thomas Husebø <thomas.husebo.90@gmail.com>
2020-02-20 16:13:26 +0100

    Added a new class for settings that should be available across different classes

---
+++ src/main/java/roborally/game/Settings.java
@@ -0,0 +1,8 @@
+package roborally.game;
+
+public class Settings {
+    public static int NUMBER_OF_PHASES = 5;
+    public static int NUMBER_OF_PLAYERS = 8;
+    public static int NUMBER_OF_FLAGS = 5;
+
+}
