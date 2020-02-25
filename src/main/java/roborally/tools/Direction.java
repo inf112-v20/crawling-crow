a0549bb311a2b64a7fa9783220dad97652ba9cc7
Thomas Husebø <thomas.husebo.90@gmail.com>
2020-02-25 16:12:40 +0100

    Added degrees to direction.
    All direction are now associated with an angle (degrees). #26

--- src/main/java/roborally/tools/Direction.java
+++ src/main/java/roborally/tools/Direction.java
@@ -3,10 +3,20 @@ package roborally.tools;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public enum Direction {
-    North,
-    South,
-    West,
-    East;
+    North(0),
+    South(180),
+    West(-90),
+    East(90);
+
+    private final int degrees;
+
+    Direction(int degrees) {
+        this.degrees = degrees;
+    }
+
+    public int getDegrees(){
+        return this.degrees;
+    }
 
     public static Direction turnRight(Direction direction) {
         if(direction == West) {
