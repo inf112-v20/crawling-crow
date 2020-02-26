bbb54de2e046de61c80ec61275ced6286437ca91
Lise <lise.johansen@student.uib.no>
2020-02-26 10:33:47 +0100

    Created Flag class that has an id and position. #27 #76 #29
    Co-authored-by: puj009 <puj009@uib.no>

---
+++ src/main/java/roborally/game/objects/Flag.java
@@ -0,0 +1,21 @@
+package roborally.game.objects;
+
+import com.badlogic.gdx.math.GridPoint2;
+
+public class Flag {
+    private int id;
+    private GridPoint2 pos;
+
+    public Flag(int id, GridPoint2 pos) {
+        this.id = id;
+        this.pos = pos;
+    }
+
+    public GridPoint2 getPos() {
+        return pos;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
