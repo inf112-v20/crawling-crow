f657c6b2c053decea659f764cb9a8367ce0f0a7c
sigmund volden <sigmundvolden@gmail.com>
2020-02-27 15:56:20 +0100

    Test Added some test functions for RobotTest

--- src/test/java/roborally/game/objects/robot/RobotTest.java
+++ src/test/java/roborally/game/objects/robot/RobotTest.java
@@ -1,5 +1,28 @@
 package roborally.game.objects.robot;
 
-public class RobotTest {
+import org.junit.Before;
+import org.junit.Test;
 
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.assertThat;
+
+public class RobotTest {
+    private Robot testRobot1;
+    private Robot testRobot2;
+    @Before
+    public void setUp() {
+        testRobot1 = new Robot(new RobotState("T1"));
+        testRobot2 = new Robot(new RobotState("T2"));
+    }
+
+    @Test
+    public void verifyThatRobot1NameNotEqualToRobot2Name() {
+        assertThat(testRobot1.getName(), not(testRobot2.getName()));
+    }
+    @Test
+    public void verifyThatRobot1PositionNotEqualToRobot2Position() {
+        testRobot1.setPos(5,5);
+        testRobot2.setPos(4,4);
+        assertThat(testRobot1.getPosition(), not(testRobot2.getPosition()));
+    }
 }
