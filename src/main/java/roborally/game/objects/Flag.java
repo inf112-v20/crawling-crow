d44455a9bf31390296c102bea28d1331e8e15ed3
Lise <lise.johansen@student.uib.no>
2020-02-28 08:34:00 +0100

    Created interface for flag class and added comments

--- src/main/java/roborally/game/objects/Flag.java
+++ src/main/java/roborally/game/objects/Flag.java
@@ -2,7 +2,7 @@ package roborally.game.objects;
 
 import com.badlogic.gdx.math.GridPoint2;
 
-public class Flag {
+public class Flag implements IFlag{
     private int id;
     private GridPoint2 pos;
 
