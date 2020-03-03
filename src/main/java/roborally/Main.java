5db60a36ac1e4a0dd09a021d97cf0d5c88daa609
Tim <tim@adipid.no>
2020-03-03 15:31:20 +0100

    Made icons for the appliation

--- src/main/java/roborally/Main.java
+++ src/main/java/roborally/Main.java
@@ -1,5 +1,6 @@
 package roborally;
 
+import com.badlogic.gdx.Files;
 import roborally.game.SettingsUtil;
 import roborally.ui.UI;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
@@ -12,6 +13,9 @@ public class Main {
         cfg.title = "RoboRally by CrawlingCrow";
         cfg.width = SettingsUtil.WINDOW_WIDTH;
         cfg.height = SettingsUtil.WINDOW_HEIGHT;
+        cfg.addIcon("icons/icon@4x.png", Files.FileType.Internal);
+        cfg.addIcon("icons/icon@2x.png", Files.FileType.Internal);
+        cfg.addIcon("icons/icon.png", Files.FileType.Internal);
 
         new LwjglApplication(new UI(), cfg);
     }
