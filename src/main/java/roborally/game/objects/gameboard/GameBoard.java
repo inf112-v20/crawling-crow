cb27358f90713a39c77ba22147ed4f8d379030e3
Tim <tim@adipid.no>
2020-03-05 09:04:50 +0100

    Removed deprecation in GameBoard
    Co-authored-by: thomashusebo <thomas.husebo.90@gmail.com>

--- src/main/java/roborally/game/objects/gameboard/GameBoard.java
+++ src/main/java/roborally/game/objects/gameboard/GameBoard.java
@@ -24,33 +24,6 @@ public class GameBoard implements IGameBoard {
     }
 
     @Override
-    public boolean onFlag(int x, int y) {
-        if(!layers.contains("Flag"))
-            return false;
-        return layers.assertFlagNotNull(x, y);
-    }
-
-    @Override
-    public boolean onHole(int x, int y) {
-        if(layers.contains("bug") && layers.assertBugNotNull(x, y))
-            return true;
-        if(!layers.contains("Hole"))
-            return false;
-        return layers.assertHoleNotNull(x, y);
-    }
-
-    @Override
-    public void setCheckpoint(Vector2 pos, AI ai) {
-        if (this.onFlag((int) pos.x, (int) pos.y)) {
-            ai.setWinTexture();
-            if(ai.getCurrFlagPos().x == pos.x && ai.getCurrFlagPos().y == pos.y)
-                ai.setFlagPos();
-        }
-        if (this.onHole((int) pos.x, (int) pos.y))
-            ai.setLostTexture();
-    }
-
-    @Override
     public ArrayList<IFlag> findAllFlags() {
         ArrayList<IFlag> flags = new ArrayList<>();
         TiledMapTileLayer flagLayer = layers.getFlag();
@@ -65,10 +38,5 @@ public class GameBoard implements IGameBoard {
             }
         }
         return flags;
-    }
-
-    @Override
-    public boolean canMove() {
-        return false;
     }
 }
