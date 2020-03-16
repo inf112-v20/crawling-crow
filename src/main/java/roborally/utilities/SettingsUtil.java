c48d9a7140add1a66bbbb849cb21ea36ee92a9ba
Tim <tim@adipid.no>
2020-03-16 15:11:14 +0100

    Fixed resolution, so it is possible to see the whole board
    Co-authored-by: thomashusebo <thomas.husebo.90@gmail.com>
    Co-authored-by: puj009 <puj009@uib.no>
    Co-authored-by: ljo071 <lise.johansen@student.uib.no>

--- src/main/java/roborally/utilities/SettingsUtil.java
+++ src/main/java/roborally/utilities/SettingsUtil.java
@@ -6,6 +6,6 @@ public class SettingsUtil {
     public static int NUMBER_OF_FLAGS = 3;
 
     public static int WINDOW_WIDTH = 900;
-    public static int WINDOW_HEIGHT = 650;
+    public static int WINDOW_HEIGHT = 675;
 
 }
