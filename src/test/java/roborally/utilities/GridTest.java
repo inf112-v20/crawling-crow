092aa2318cbaeb32395b019aa4084e026fd9583e
Tim <tim@adipid.no>
2020-04-02 14:55:11 +0200

    Added more flag tests

--- src/test/java/roborally/utilities/GridTest.java
+++ src/test/java/roborally/utilities/GridTest.java
@@ -1,6 +1,6 @@
 package roborally.utilities;
 
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import roborally.utilities.enums.LayerName;
 import roborally.utilities.enums.TileName;
@@ -8,17 +8,30 @@ import roborally.utilities.enums.TileName;
 import static org.junit.Assert.assertTrue;
 
 public class GridTest {
-    private Grid grid;
+    private static Grid newMap;
+    private static Grid riskyExchange;
 
-    @Before
-    public void setUp() {
-        grid = new Grid("/maps/newmap.tmx");
+    @BeforeClass
+    public static void setUp() {
+        newMap = new Grid("/maps/newmap.tmx");
+        riskyExchange = new Grid("/maps/riskyExchangeBeginnerWithStartAreaVertical.tmx");
     }
 
     @Test
-    public void flag1FoundInGrid() {
+    public void verifyThatFlag1IsInGrid() {
         TileName tileName = TileName.FLAG_1;
-        assertTrue(grid.getGridLayer(LayerName.FLAG).containsValue(tileName));
+        assertTrue(newMap.getGridLayer(LayerName.FLAG).containsValue(tileName));
     }
 
+    @Test
+    public void verifyThatFlag2IsInGrid() {
+        TileName tileName = TileName.FLAG_2;
+        assertTrue(newMap.getGridLayer(LayerName.FLAG).containsValue(tileName));
+    }
+
+    @Test
+    public void verifyThatFlag3IsInGrid() {
+        TileName tileName = TileName.FLAG_3;
+        assertTrue(newMap.getGridLayer(LayerName.FLAG).containsValue(tileName));
+    }
 }
