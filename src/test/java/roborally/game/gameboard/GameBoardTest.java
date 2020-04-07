cf1566f44c7562ca0be3f162b95de06a9b08fbdc
Lise <lise.johansen@student.uib.no>
2020-04-07 09:54:35 +0200

    Added two tests to check if the gameboard has conveyorbelts. Both normal and express.

--- src/test/java/roborally/game/gameboard/GameBoardTest.java
+++ src/test/java/roborally/game/gameboard/GameBoardTest.java
@@ -23,6 +23,16 @@ public class GameBoardTest {
 	}
 
 	@Test
+	public void hasNormalConveyorBelts() {
+		assertTrue(gameBoard.findAllNormalConveyorBelts().size() > 0);
+	}
+
+	@Test
+	public void hasExpressConveyorBelts() {
+		assertTrue(gameBoard.findAllExpressConveyorBelts().size() > 0);
+	}
+
+	@Test
 	public void hasPushers() {
 		assertTrue(gameBoard.hasPushers());
 	}
