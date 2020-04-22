40ccc6f9ca57ecce0f48edfd999a753e2289f935
Tim <tim@adipid.no>
2020-04-22 10:34:40 +0200

    Window size is now determined by users resolution

--- src/main/java/roborally/Main.java
+++ src/main/java/roborally/Main.java
@@ -1,16 +1,19 @@
 package roborally;
 
 import com.badlogic.gdx.Files;
-import roborally.utilities.SettingsUtil;
-import roborally.ui.UI;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import roborally.ui.UI;
+import roborally.utilities.SettingsUtil;
 
 
 public class Main {
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
         cfg.title = "RoboRally by CrawlingCrow";
+        /*GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        int width = gd.getDisplayMode().getWidth();
+        int height = gd.getDisplayMode().getHeight();*/
         cfg.width = SettingsUtil.WINDOW_WIDTH;
         cfg.height = SettingsUtil.WINDOW_HEIGHT;
         cfg.addIcon("icons/icon@4x.png", Files.FileType.Internal);
--- src/main/java/roborally/utilities/SettingsUtil.java
+++ src/main/java/roborally/utilities/SettingsUtil.java
@@ -2,6 +2,8 @@ package roborally.utilities;
 
 import com.badlogic.gdx.math.GridPoint2;
 
+import java.awt.*;
+
 public class SettingsUtil {
     //region Game Settings
     public static int NUMBER_OF_PHASES = 5;
@@ -16,8 +18,10 @@ public class SettingsUtil {
 
     public static GridPoint2 GRAVEYARD = new GridPoint2(-1,-1);
 
-    public static int WINDOW_WIDTH = 1920;
-    public static int WINDOW_HEIGHT = 1080;
+    private static GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+
+    public static int WINDOW_WIDTH = gd.getDisplayMode().getWidth();
+    public static int WINDOW_HEIGHT = gd.getDisplayMode().getHeight();
     public static float UNIT_SCALE = 3 / 16f;
     public static float TILE_SCALE = UNIT_SCALE * 300;
     public static float MAP_WIDTH = TILE_SCALE * 16; // needs to be generic if we want maps with different sizes.
