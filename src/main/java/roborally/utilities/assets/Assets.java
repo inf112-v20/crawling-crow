9f344d25a239ea2983e49baf8f9c92c60127926c
buzzdead <sigmundvolden@gmail.com>
2020-05-03 11:25:47 +0200

    Added documentation.

--- src/main/java/roborally/utilities/assets/Assets.java
+++ src/main/java/roborally/utilities/assets/Assets.java
@@ -4,8 +4,16 @@ import com.badlogic.gdx.assets.AssetManager;
 
 public interface Assets {
 
+	/**
+	 * Loads all the assets into the manager.
+	 * @param manager the AssetManager.
+	 */
 	void loadAssets(AssetManager manager);
 
+	/**
+	 * Puts all the assets into a hashmap for creating access to them.
+	 * @param manager the AssetManager.
+	 */
 	void putAssetsInMap(AssetManager manager);
 
 }
