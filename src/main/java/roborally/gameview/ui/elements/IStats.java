cfac1bfc97f68d4673981089efbf2afc72b1541b
Tim <tim@adipid.no>
2020-05-06 14:18:50 +0200

    Added javadoc #162 #209

--- src/main/java/roborally/gameview/ui/elements/IStats.java
+++ src/main/java/roborally/gameview/ui/elements/IStats.java
@@ -5,6 +5,13 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import java.util.ArrayList;
 
 public interface IStats {
+    /**
+     * @return List of images
+     */
     ArrayList<Image> get();
+
+    /**
+     * Clears list of images
+     */
     void clear();
 }
