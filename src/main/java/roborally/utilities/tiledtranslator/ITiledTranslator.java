235f206df81c183ca494e7a317ff8da0a8f06127
Lise <lise.johansen@student.uib.no>
2020-05-07 11:30:53 +0200

    #207 Updated javadoc

--- src/main/java/roborally/utilities/tiledtranslator/ITiledTranslator.java
+++ src/main/java/roborally/utilities/tiledtranslator/ITiledTranslator.java
@@ -3,6 +3,7 @@ package roborally.utilities.tiledtranslator;
 import roborally.utilities.enums.TileName;
 
 public interface ITiledTranslator {
+
     /**
      * @param tiledID The int ID of the Tile-element
      * @return The name of the tile as TileName enum using the corresponding tileID
